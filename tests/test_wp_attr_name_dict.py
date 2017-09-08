af93c160b7801c6e7add97c430a8937f1d81063c
wuyu <yu.wu@mso-china.com>
2017-09-08 18:33:35 +0800

    add test: wp-attr-name-dict

---
+++ tests/test_wp_attr_name_dict.py
@@ -0,0 +1,41 @@
+import unittest
+
+import weakscraper
+from weakscraper import exceptions
+
+
+class TestWPAttrNameDict(unittest.TestCase):
+    def setUp(self):
+        template_string = """
+            <!DOCTYPE html>
+            <html>
+                <body wp-name='body'>
+                    <a wp-attr-name-dict="{'href': 'url', 'class': 'class_name', 'howto': 'other'}" />
+                </body>
+            </html>
+            """
+
+        self.scraper = weakscraper.WeakScraper(template_string)
+
+    def test_match(self):
+        content = """
+            <!DOCTYPE html>
+            <html>
+                <body>
+                    <a href='http://www.163.com' class='test_01' trage='_blank'>test</a>
+                </body>
+            </html>
+            """
+
+        result_data = self.scraper.scrap(content)
+
+        info = {
+                "body": {
+                    "url": "http://www.163.com",
+                    "class_name": "test_01",
+                },
+            }
+
+        self.assertEqual(result_data, info)
+
+
