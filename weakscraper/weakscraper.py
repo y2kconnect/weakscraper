243bc86d19b57b1f4a5871be557b76a61a4543b5
wuyu <yu.wu@mso-china.com>
2017-10-17 11:25:52 +0800

    代码优化

--- weakscraper/weakscraper.py
+++ weakscraper/weakscraper.py
@@ -3,10 +3,7 @@
 '''
 
 # python apps
-import collections
 import bs4
-import json
-import sys
 
 # our apps
 from weakscraper.template import Template
@@ -39,50 +36,3 @@ class WeakScraper:
         return results
 
 
-def node_to_json(node, arr_key):
-    '节点转JSON'
-    info = {'nodetype': node.__class__.__name__}
-    if isinstance(node, bs4.NavigableString):
-        info['content'] = str(node.string)
-    else:
-        for s in arr_key:
-            x = getattr(node, s, None)
-            if x:
-                info[s] = x
-    return info
-
-
-def serialize(root, arr_key=('name', 'attrs', 'params', 'children')):
-    '序列化DOM树, 深度遍历'
-    arr_tree = []
-    arr_node = collections.deque()
-    arr_node.append((root, arr_tree))
-
-    while arr_node:
-        node, arr_ret = arr_node.popleft()
-        info = node_to_json(node, arr_key)
-
-        k = 'children'
-        if k in arr_key and k in info and info[k]:
-            arr_children = []
-            arr = [(node, arr_children) for node in info[k]]
-            arr_node.extendleft(reversed(arr))
-            info[k] = arr_children
-
-        arr_ret.append(info)
-
-    n = len(arr_tree)
-    if n == 0:
-        ret = None
-    elif n == 1:
-        ret = arr_tree[0]
-    else:
-        ret = arr_tree
-    return ret
-
-
-def show_DOM(root, label='root_tree', stream=sys.stdout, indent=4):
-    '显示DOM树'
-    arr_tree = serialize(root)
-    msg = json.dumps(arr_tree, ensure_ascii=False, indent=indent)
-    print('{label}:\n{msg}'.format(label=label, msg=msg), file=stream)
