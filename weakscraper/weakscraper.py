96800cbc5a75143f5c07a972ef8eacea70419aed
wuyu <yu.wu@mso-china.com>
2017-10-25 15:22:00 +0800

    代码格式优化

--- weakscraper/weakscraper.py
+++ weakscraper/weakscraper.py
@@ -1,39 +1,43 @@
 # -*- encoding: utf-8 -*-
 
 # our apps
-from .template import Template
 from .htmlparser import HtmlParser
+from .template import Template
 from .templateparser import TemplateParser
-from .utils import show_DOM, serialize
+from .utils import serialize
 
 
 class WeakScraper:
     def __init__(self, template_string, functions=None, debug=False):
         self.debug = debug
-        template_parser = TemplateParser()
-        template_parser.feed(template_string)
-        tpl_tree = template_parser.get_result()
-        self.info = {'tpl_tree': tpl_tree}
 
-        self.template = Template(tpl_tree, functions, debug)
-        self.info['Template_tree'] = serialize(self.template)
+        parser_template = TemplateParser()
+        parser_template.feed(template_string)
+        tree_tpl = parser_template.get_result()
+        self.info = {'tree_tpl': tree_tpl}
+
+        # 处理标签的"wp-*"属性
+        self.template = Template(tree_tpl, functions, debug)
+        self.info['tree_Template'] = serialize(self.template)
 
     def scrap(self, html):
-        html_parser = HtmlParser()
-        html_parser.feed(html)
-        html_tree = html_parser.get_result()
-        self.info['html_tree'] = html_tree
+        parser_html = HtmlParser()
+        parser_html.feed(html)
+        tree_html = parser_html.get_result()
+        self.info['tree_html'] = tree_html
 
         if self.debug:
             import json
-            s = '\n{sep}\ntree_html:\n{t_html}\n{sep}\ntree_tpl:\n{t_tpl}\n{sep}'
+            s = '\n{sep}\ntree_html:\n{tree_html}\n{sep}\ntree_tpl:\n{tree_tpl}\n{sep}'
             info = dict(ensure_ascii=False, indent=4)
             sep = '\n' + '-' * 40
             print(s.format(
                     sep=sep,
-                    t_html=json.dumps(self.info['html_tree'], **info),
-                    t_tpl=json.dumps(self.info['tpl_tree'], **info),
+                    tree_html=json.dumps(self.info['tree_html'], **info),
+                    tree_tpl=json.dumps(self.info['tree_tpl'], **info),
                     ))
 
-        results = self.template.compare(html_tree)
+        results = self.template.compare(tree_html)
+        self.info['results'] = results
+
         return results
