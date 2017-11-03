5abb9d400e857816629e44ae51074b92823e8e55
wuyu <yu.wu@mso-china.com>
2017-11-03 18:04:29 +0800

    template.py函数调用示意图

--- tests/test_wpnugget.py
+++ tests/test_wpnugget.py
@@ -7,11 +7,10 @@ class TestWPNugget(unittest.TestCase):
     def setUp(self):
         template_string = """
             <!DOCTYPE html>
-            <html>
-                <body>
-                    <tag1>begining<wp-nugget wp-name="info1"/>middle<wp-nugget wp-name="info2"/>end</tag1>
-                    <tag2><wp-nugget wp-name="info3"/>end</tag2>
-                </body>
+            <body>
+              <tag1>begining<wp-nugget wp-name="info1"/>middle<wp-nugget wp-name="info2"/>end</tag1>
+              <tag2><wp-nugget wp-name="info3"/>end</tag2>
+            </body>
             </html>
             """
 
@@ -21,11 +20,10 @@ class TestWPNugget(unittest.TestCase):
     def test_1(self):
         content = """
             <!DOCTYPE html>
-            <html>
-                <body>
-                    <tag1>beginingABCmiddleDEFend</tag1>
-                    <tag2>GHIend</tag2>
-                </body>
+            <body>
+              <tag1>beginingABCmiddleDEFend</tag1>
+              <tag2>GHIend</tag2>
+            </body>
             </html>
             """
 
